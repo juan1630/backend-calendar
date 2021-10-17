1068c9dad803cfb37674f4b8a5f66546f61b133b
Juan Patrón <josejuanpatron1630@gmail.com>
2021-10-17 15:21:11 -0500

    Se creo el cifrado de las contrasenias

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,48 @@
 const { response} = require('express');
+const Usuario = require('../models/User');
+const bcrypt = require('bcryptjs');
+
+const createUser = async (req, res=response) => {
 
 
-const createUser = (req, res=response) => {
-    
     const { email, password } = req.body;
 
+    try {
+        
+        let usuario = await Usuario.findOne({ email});
+        
+        if(usuario) {
+            return res.status(400)
+                .json({ 
+                    ok: false,
+                    msg: 'El correo ya ha sido registrado' 
+
+                })
+        }
+
+        usuario = new Usuario(req.body);
+            // recive el numero de vuletas que le hara a la contraseña
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync( password, salt );
+
+         await usuario.save();
    
-    //validamos si es que hay errores retorne los errores
- 
-  
-    res.status(201).json({
-        ok: true,
-        message: 'Register',
-        email, 
-        password 
-    });
+           res.status(201).json({
+            
+            ok: true,
+            uid: usuario._id,
+            name: usuario.name,
+       });
+
+    } catch (error) {
+        console.error(error);
+        
+        res.status(500).json({
+            ok: false,
+            msg: 'No se creo'
+        });
+    }
+
 }
 
 
