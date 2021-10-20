1f5225ec878fc549840e266a657830656bb2cd86
Juan Patrón <josejuanpatron1630@gmail.com>
2021-10-19 19:26:18 -0500

    SE agrego el revalidte de los tokens

---
+++ middlewares/validar-Jwt.js
@@ -0,0 +1,37 @@
+const { response, request } = require('express');
+const jwt = require('jsonwebtoken');
+
+
+const validarJwt = (req = request, resp =response ,next) => {
+
+    //leemos el token que se envia por el req
+    const  token  = req.header('x-token');
+
+    if(!token){
+        return resp.status(401)
+        .json({ 
+            ok: false,
+            msg : 'No tienes las credenciales'
+        });
+    }
+
+    try {
+        //hacemos el destructuring de los datos
+        const {uid, name} = jwt.verify(  token,  process.env.SECRET_JWT_SEED );
+        //seteamos los datos al request
+        req.uid =  uid;
+        req.name = name;
+        
+    } catch (error) {
+        return resp.status(401).json({ 
+                                        ok: false, 
+                                        msg: 'Token no valido'
+                                    });
+    }
+    
+    next();
+}
+
+
+module.exports = {validarJwt};
+
