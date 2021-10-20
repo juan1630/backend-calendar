547d6a4ef1aef984cf8fce604859eb0fd7a05a2d
Juan Patrón <josejuanpatron1630@gmail.com>
2021-10-19 21:34:25 -0500

    Se agregaron las rutas del crud de los eventos

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const express = require('express');
 const { connectToDB } = require('./database/db-conf');
 const app = express();
-
+const cors = require('cors');
 // importamos el env
 require('dotenv').config();
 
+//conexion a la DB
+connectToDB();
+
+app.use( cors() );
 // midleware de los archivos publicos
 // Directorio publico 
 app.use( express.static('public'))
 
-//conexion a la DB
-
-connectToDB();
 
 //  Parseo de los json a un objeto vaido
 app.use( express.json());
 // Rutas de autenticacion
 app.use('/api/auth', require('./routes/auth'));
 
+// Ruta del crud de los eventos
+app.use('/api/events', require('./routes/events')); 
 
 app.listen( process.env.PORT  , ()=> {
     console.log('listening on port ' + process.env.PORT)
